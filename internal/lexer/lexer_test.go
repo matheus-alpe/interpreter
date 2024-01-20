7d30e8e3fc643cda12e28cfd834e6ece415acd14
Matt <matttalves@gmail.com>
2024-01-20 09:46:04 -0300

    test: more complex input

--- internal/lexer/lexer_test.go
+++ internal/lexer/lexer_test.go
@@ -14,11 +14,11 @@ type TokenTestExpected struct {
 func TestSimpleNextToken(t *testing.T) {
 	input := `=+(){},;`
 
-	tests := []TokenTestExpected {
+	tests := []TokenTestExpected{
 		{token.ASSIGN, "="},
 		{token.PLUS, "+"},
 		{token.LPARENT, "("},
-		{token.RPARENT, "="},
+		{token.RPARENT, ")"},
 		{token.LBRACE, "{"},
 		{token.RBRACE, "}"},
 		{token.COMMA, ","},
@@ -26,21 +26,80 @@ func TestSimpleNextToken(t *testing.T) {
 		{token.EOF, ""},
 	}
 
-    l := New(input)
+	l := New(input)
 
-    for i, tt := range tests {
-        tok := l.NextToken()
-	checkToken(t, i, &tok, &tt)
-    }
+	for i, tt := range tests {
+		tok := l.NextToken()
+		checkToken(t, i, &tok, &tt)
+	}
+}
+
+func TestComplexNextToken(t *testing.T) {
+	input := `let five = 5;
+	let ten = 10;
+
+	let add = fn(x, y) {
+		x + y;
+	};
+
+	let result = add(five, ten);
+	`
+
+	tests := []TokenTestExpected{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPARENT, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPARENT, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPARENT, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPARENT, ")"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+		checkToken(t, i, &tok, &tt)
+	}
 }
 
 func checkToken(t *testing.T, i int, current *token.Token, expected *TokenTestExpected) {
 	t.Helper()
-        if current.Type != expected.Type {
-            t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, expected.Type, current.Type)
-        }
+	if current.Type != expected.Type {
+		t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, expected.Type, current.Type)
+	}
 
-        if current.Literal != expected.Literal {
-            t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, expected.Type, current.Type)
-        }
+	if current.Literal != expected.Literal {
+		t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, expected.Literal, current.Literal)
+	}
 }
