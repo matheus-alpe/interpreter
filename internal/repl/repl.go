124672ee5015566b1368a66b74372122630f8ce7
Matt <matttalves@gmail.com>
2024-01-24 22:56:08 -0300

    feat: init repl

---
+++ internal/repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/matheus-alpe/interpreter/internal/lexer"
+	"github.com/matheus-alpe/interpreter/internal/token"
+)
+
+const PROMPT = "> "
+
+func Start(in io.Reader) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Print(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%v\n", tok)
+		}
+	}
+}
