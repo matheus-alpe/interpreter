e6021082a11d07ca97b9976e985dbd2a7953e7ec
Matt <matttalves@gmail.com>
2024-01-24 23:20:28 -0300

    feat: main

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/matheus-alpe/interpreter/internal/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s, welcome to Shitscript!\n", user.Username)
+	repl.Start(os.Stdin)
+}
