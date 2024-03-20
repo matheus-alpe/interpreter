b92bc2233c819114a6106e5ee6cee8c6db54fb59
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2024-03-20 13:46:51 -0300

    feat: evaluate booleans

--- internal/evaluator/evaluator.go
+++ internal/evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"github.com/matheus-alpe/interpreter/internal/object"
 )
 
+// Created to not instantiate every time has a bool value, because only have 2 possibles values.
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -13,6 +19,8 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObjecT(node.Value)
 	}
 
 	return nil
@@ -26,4 +34,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObjecT(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
