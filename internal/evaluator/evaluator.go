43a3161d264502bee9a7bb062c769f3b67464e5f
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2024-03-20 17:58:22 -0300

    feat: evaluate boolean infix expression

--- internal/evaluator/evaluator.go
+++ internal/evaluator/evaluator.go
@@ -80,6 +80,10 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBoolToBooleanObjecT(left == right) // pointer comparison to check equality between booleans
+	case operator == "!=":
+		return nativeBoolToBooleanObjecT(left != right)
 	default:
 		return NULL
 	}
