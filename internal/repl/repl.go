8986838862317a4691871b8f78d795577a9a6f1b
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2024-03-20 13:25:08 -0300

    feat: repl with evaluation

--- internal/repl/repl.go
+++ internal/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/matheus-alpe/interpreter/internal/evaluator"
 	"github.com/matheus-alpe/interpreter/internal/lexer"
 	"github.com/matheus-alpe/interpreter/internal/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
